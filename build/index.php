1a5266776c6d3541c3ef89e34a47790d95f353e0
Charles Hopkins <charleshopkinsiv@gmail.com>
2019-07-17 01:18:01 -0700

    Beta Launch

--- build/index.php
+++ build/index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" type="text/css" href="style.css">
-  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css"> -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
   <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Noto+Sans&display=swap" rel="stylesheet">
   <title>Map Chat</title>
 </head>
@@ -64,7 +64,7 @@
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBnkXfIl1sDabZcnzEdYgy9X-fAXIYcnY4&callback=initMap"
   async defer></script>
 
-  <script src="app.js"></script>
+  <script src="app-min.js"></script>
 
 </body>
 </html>
